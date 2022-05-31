ea4baaacbae76aabaabc6c92aedc4f468a58636e
phikieu <phi.kieu.insenco@gmail.com.vn>
2022-05-31 23:16:57 +0700

    Add UserController and swagger

---
+++ source/WebServiceBooking.Backend/Controllers/UsersController.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using WebServiceBooking.Backend.Data;
+using WebServiceBooking.Backend.Data.Entities;
+using WebServiceBooking.ViewModels.Contents;
+
+namespace WebServiceBooking.Backend.Controllers
+{
+    public class UsersController : BaseController
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly WebDBContext _context;
+        public UsersController(UserManager<User> userManager, WebDBContext webDBContext)
+        {
+            this._userManager = userManager;
+            this._context = webDBContext;
+
+        }
+
+        // POST: api/user
+        [HttpPost]
+        public async Task<IActionResult> PostUser(UserCreateRequest request)
+        {
+            var user = new User()
+            {
+                Id = Guid.NewGuid().ToString(),
+                Email = request.Email,
+                Dob = DateTime.Parse(request.Dob),
+                UserName = request.UserName,
+                LastName = request.LastName,
+                FirstName = request.FirstName,
+                PhoneNumber = request.PhoneNumber,
+                CreateDate = DateTime.Now,
+            };
+            var result = await _userManager.CreateAsync(user, request.Password);
+            if (result.Succeeded)
+            {
+                return CreatedAtAction(nameof(GetById), new { id = user.Id }, request);
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+        // GET: api/user
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(string id)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+            if (user == null)
+                return NotFound();
+
+            var userVm = new UserVm()
+            {
+                Id = user.Id,
+                UserName = user.UserName,
+                Dob = user.Dob,
+                Email = user.Email,
+                PhoneNumber = user.PhoneNumber,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                CreateDate = user.CreateDate
+            };
+            return Ok(userVm);
+        }
+
+        // GET: api/user
+        [HttpGet]
+        public async Task<IActionResult> GetUsers()
+        {
+            var users = _userManager.Users;
+
+            var uservms = await users.Select(u => new UserVm()
+            {
+                Id = u.Id,
+                UserName = u.UserName,
+                Dob = u.Dob,
+                Email = u.Email,
+                PhoneNumber = u.PhoneNumber,
+                FirstName = u.FirstName,
+                LastName = u.LastName,
+                CreateDate = u.CreateDate
+            }).ToListAsync();
+
+            return Ok(uservms);
+        }
+
+        // PUT: api/user
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutUser(string id, [FromBody] UserCreateRequest request)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+            if (user == null)
+                return NotFound();
+
+            user.FirstName = request.FirstName;
+            user.LastName = request.LastName;
+            user.Dob = DateTime.Parse(request.Dob);
+            user.LastModifiedDate = DateTime.Now;
+
+            var result = await _userManager.UpdateAsync(user);
+
+            if (result.Succeeded)
+            {
+                return NoContent();
+            }
+            return BadRequest();
+        }
+    }
+}
--- source/WebServiceBooking.Backend/Startup.cs
+++ source/WebServiceBooking.Backend/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -23,7 +24,9 @@ namespace WebServiceBooking.Backend
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
             services.AddRazorPages();
+            services.AddSwaggerGen(c => c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" }));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -47,8 +50,11 @@ namespace WebServiceBooking.Backend
 
             app.UseAuthorization();
 
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1"));
+
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapDefaultControllerRoute();
                 endpoints.MapRazorPages();
             });
         }
